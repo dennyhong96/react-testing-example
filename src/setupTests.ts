be821173cbbd547785f88af1c9bf4fb26e31c612
Denny Hong <hong961127@gmail.com>
2021-08-14 13:37:55 -0400

    Setup msw

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,15 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+
+import { server } from "./mocks/server";
+
+// Start mock server
+beforeAll(() => server.listen());
+
+// Reset any request handlers that we may add during the tests,
+// so they don't affect other tests.
+afterEach(() => server.resetHandlers());
+
+// Clean up, close mock server
+afterAll(() => server.close());
